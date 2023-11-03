ff9e610cfd03e2b3da559c14939882db13bb9ec2
Vitalii Kotelianets <kotelyanets97@gmail.com>
2023-11-03 20:56:22 +0200

    finished page for desktop screens

---
+++ src/components/CTA/CTA.js
@@ -0,0 +1,19 @@
+import './CTA.css';
+
+const CTA = () => {
+  return (
+    <div className="cta">
+      <div className="cta__content">
+        <div className="cta__content--top">
+          Request Early Access to Get Started
+        </div>
+        <div className="cta__content--main">
+          Register today & start exploring the endless possiblities.
+        </div>
+      </div>
+      <button className="cta__button">Get Started</button>
+    </div>
+  );
+};
+
+export default CTA;
